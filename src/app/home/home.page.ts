b1a68bf5e65dd1ebe0f2ea1e94bff786f6680ee5
wasudo <wasd2k6@gmail.com>
2019-07-23 02:33:09 +0800

    login and send message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,38 +8,49 @@ import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 })
 export class HomePage {
 
+  loginData: {};
+  loginSuccess: boolean;
+
   constructor(private fb: Facebook) { }
+
+  sendMessage() {
+      this.fb.showDialog({
+                method: 'send',
+                caption: 'Check this out.',
+                link: 'http://example.com',
+                description: 'The site I told you about',
+                picture: 'http://example.com/image.png'
+            }
+        );
+  }
 
   loginAction() {
       // Login with permissions
-      this.fb.login(['public_profile', 'user_photos', 'email', 'user_birthday'])
+      this.fb.login(['public_profile', 'email'])
       .then( (res: FacebookLoginResponse) => {
 
           // The connection was successful
           if (res.status === 'connected') {
 
               // Get user ID and Token
-              let fb_id = res.authResponse.userID;
-              let fb_token = res.authResponse.accessToken;
+              // let fb_id = res.authResponse.userID;
+              // let fb_token = res.authResponse.accessToken;
 
               // Get user infos from the API
-              this.fb.api('/me?fields=name,gender,birthday,email', []).then((user) => {
+              this.fb.api('/me?fields=name,id', []).then((user) => {
 
                   // Get the connected user details
-                  let gender    = user.gender;
-                  let birthday  = user.birthday;
-                  let name      = user.name;
-                  let email     = user.email;
+                  let name = user.name;
 
-                  console.log('=== USER INFOS ===');
-                  console.log('Gender : ' + gender);
-                  console.log('Birthday : ' + birthday);
                   console.log('Name : ' + name);
-                  console.log('Email : ' + email);
-                  // => Open user session and redirect to the next page
+                  this.loginData = {
+                      name: user.name,
+                      id: user.id
+                  };
+
+                  this.loginSuccess = true;
               });
-          }
-          else {
+          } else {
               console.log('An error occurred...');
           }
       })
